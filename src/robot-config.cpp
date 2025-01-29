14c2d3639893257391242e6a89206345a447e79d
Shri Krishna Sivakumar <104104083+IntelligentLion@users.noreply.github.com>
2025-01-28 17:42:50 -0800

    intake and elevator motors added

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -12,13 +12,20 @@ brain  Brain;
 //motor LeftFront = motor(PORT1, ratio6_1, false);
 
 //Add your devices below, and don't forget to do the same in robot-config.h:
+
+// Left Motors:
 motor LeftFront = (1, ratio6_1, true); 
 motor LeftMiddle = (2, ratio6_1, true);
 motor LeftBack = (3, ratio6_1, true); 
 
+// Right Motors: 
 motor RightFront = (8, ratio6_1, false);
 motor RightMiddle = (9, ratio6_1, false);
 motor RightBack = (10, ratio6_1, false);
+
+// Other Motors:
+motor Intake = (6, ratio6_1, false);
+motor Elevator = (7, ratio2_1, false);
 
 void vexcodeInit( void ) {
   // nothing to initialize
